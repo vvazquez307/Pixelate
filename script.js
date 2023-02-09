402f287b9b89572756eaa76b97508727936fc7d2
victor <vvazquez307@gmail.com>
2023-02-09 17:11:11 -0600

    2/9/23 5:11pm

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // Your code here
+const table = document.getElementsByTagName("table")[0]
+const addRow = document.getElementById("add-row")
+const select = document.getElementsByTagName("select")[0]
+
+addRow.addEventListener('click', makeRow)
+
+function makeRow(){
+    const row = document.createElement('tr')
+    for(let i = 0; i < 20; i++){
+        const td = document.createElement('td')
+        row.appendChild(td)
+    }
+    table.appendChild(row)
+}
+
+table.addEventListener('click', colorize)
+
+function colorize(event){
+console.log(event)
+event.target.className
+if(event.target.className.length){
+    event.target.className = ""
+}else {
+    event.target.className = 'red'
+}
+}
+
+select.addEventListener('change', callback)
+
+function callback(event){
+event.target.className = event.target.value
+}
